5c7dab8e061eca6711ced75ff58544210d8ff46d
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-07-01 12:49:36 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+let event = "birthday"
+function writeCards(names,event) {
+  let cards = []
+
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    cards.push(message);
+  }
+  return cards;
+ 
+
+}
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  console.log(writeCards)
